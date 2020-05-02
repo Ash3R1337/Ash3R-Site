dc25f17305b3ad8bdd334223eb34b0b40d8a89c5
Ash3R1337 <54432714+Ash3R1337@users.noreply.github.com>
2020-05-02 10:08:26 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,4 +1,3 @@
-if (window.matchMedia("(min-width: 480px)").matches) {
 $(function() {
 
  $(window).scroll(function() {
@@ -113,7 +112,7 @@ $(function() {
 		$('.award').fadeOut(300);
 	});
 });
-}
+if (window.matchMedia("(min-width: 480px)").matches) {
 $(function() {
 	$('#prog-1').mouseover(function() 
 	{
@@ -199,118 +198,5 @@ $(function() {
 
 }
 else {
-	$(function() {
-
- $(window).scroll(function() {
-
- 	if($(this).scrollTop() != 0) 
- 	{
- 		$('#toTop').fadeIn();
- 	} 
- 	else 
- 	{
- 		$('#toTop').fadeOut();
- 	}
- });
- 
- $('#toTop').click(function() 
- {
- 	$('body,html').animate({scrollTop:0},800);
- });
- 
-});
-
-$(function() {
-
-$('#arrow-down').click(function() {
-	$('#top-bar').slideDown(200);
-	$('#arrow-down').hide();
-	$('#arrow-up').slideToggle(200);
-});
-
-$('#arrow-up').click(function() {
-	$('#top-bar').slideUp(200);
-	$('#arrow-down').slideToggle(200);
-	$('#arrow-up').hide();
-});
-
-$(window).scroll(function(){
-if($(window).scrollTop()>150){
-$('.about').fadeIn(600);
-} 
-else if($(window).scrollTop()<150){
-	$('.about').fadeOut();
-}
-
-});
-
-$(window).scroll(function(){
-if($(window).scrollTop()>850){
-$('.services-block').fadeIn(600);
-} 
-else if($(window).scrollTop()<850){
-	$('.services-block').fadeOut();
-}
-
-});
-
-$(window).scroll(function(){
-if($(window).scrollTop()>1700){
-$('.skills-block').fadeIn(600);
-} 
-else if($(window).scrollTop()<1700){
-	$('.skills-block').fadeOut();
-}
-
-});
-
-$(window).scroll(function(){
-if($(window).scrollTop()>2410){
-$('.works-block').fadeIn(600);
-} 
-else if($(window).scrollTop()<2410){
-	$('.works-block').fadeOut();
-}
-
-});
-
-});
-
-$(function() {
-	$('#about').click(function() 
- {
- 	$('body,html').animate({scrollTop: $('.about').height()},800);
- });
-
-	$('#services').click(function() 
- {
- 	$('body,html').animate({scrollTop:1500},800);
- });
-
-	$('#skills').click(function() 
- {
- 	$('body,html').animate({scrollTop:2300},800);
- });
-
-	$('#projects').click(function() 
- {
- 	$('body,html').animate({scrollTop:3100},800);
- });
-});
-
-$(function() {
-	$('.img-main img').mouseover(function() 
-	{
-		$('.lent').fadeIn(300);
-		$('.lent').animate({top: -350},300);
-		$('.award').fadeIn(300);
-	});
-
-	$('.img-main img').mouseout(function() 
-	{
-		$('.lent').fadeOut(300);
-		$('.lent').animate({top: -430});
-		$('.award').fadeOut(300);
-	});
-});
+	
 }
